8ff23b4690aebd3402d064b513bdfd05a7774a45
simoncodrington <scodrington@networkten.com.au>
2025-05-01 13:57:35 +1000

    Refactored code to split the functionality into front-end and back-end code, extracting away the API details into the server code

--- backend/server.js
+++ backend/server.js
@@ -9,24 +9,48 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-   res.json({message: "Hello!"})
+    res.json({message: "Hello there!!"})
 });
 
-app.get('/api/data', (req, res) => {
+app.get('/api/recipes', async (req, res) => {
+
+    const {query} = req.query;
+
     // Use your API key securely here
-    const apiID = process.env.API_ID;
-    const appKey = process.env.APP_KEY;
-    // Fetch data from the third-party API using the apiKey
-    /*
-    res.json({
-        apiID: apiID,
-        appKey: appKey
-    });*/
-    //res.json({ message: 'Data fetched successfully' });
-    res.json({
-        apiID: apiID,
-        appKey: appKey
-    })
+    let apiId = process.env.API_ID;
+    let appKey = process.env.APP_KEY;
+    let baseUrl = process.env.API_BASE_URL;
+
+    const headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Edamam-Account-User': process.env.API_EDAMAM_ACCOUNT
+    }
+
+    let requestUrl = `${baseUrl}?q=${query}&app_id=${apiId}&app_key=${appKey}&type=public`;
+    let data = {};
+
+    //fetch from the API recipes that relate to those ingredients
+    try {
+        const response = await fetch(requestUrl, {
+            'headers': headers,
+            'method': 'GET'
+        })
+
+        if (!response.ok) {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+        }
+
+        data = await response.json();
+    } catch (error) {
+        throw new Error(`HTTP error! Status: ${error.message}`);
+    }
+
+    //return the response
+    return res.json(data);
+
 });
 
 app.listen(PORT, () => {
--- frontend/src/components/RecipeGenerator.jsx
+++ frontend/src/components/RecipeGenerator.jsx
@@ -12,77 +12,40 @@ const RecipeGenerator = ({ingredients}) => {
     function getRecipes(e) {
         e.preventDefault();
         fetchRecipes(ingredients);
-
     }
 
-    function testButton(e){
-        e.preventDefault();
-
-        fetch('/api/data')
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    }
 
     //called from the button to load more, will use the nextrecipe link to load the next set of data
-    function getMoreRecipes(e){
+    function getMoreRecipes(e) {
         e.preventDefault();
         fetchRecipes(ingredients, nextRecipesLink)
     }
 
     //fetch recipes from the API
-    function fetchRecipes(ingredients, nextLink = null) {
+    async function fetchRecipes(ingredients, nextLink = null) {
 
-        let searchterm = ingredients.join(" ");
+        let searchTerms = ingredients.join(" ");
+        let encodedSearchTerm = encodeURIComponent(searchTerms);
 
-        const headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Edamam-Account-User': 'scodrington'
-        }
-        let baseUrl = "https://api.edamam.com/api/recipes/v2";
-        let appId = "4e1c40ce";
-        let appKey = "cdec51cf61393d7a2a90f499218fa03c";
+        //get data from backend API
+        const request = await fetch(`/api/recipes?query=${encodedSearchTerm}`);
+        const data = await request.json()
 
-        //no next link, build url
-        let requestUrl = '';
-        if(nextLink == null){
-            requestUrl = `${baseUrl}?q=${searchterm}&app_id=${appId}&app_key=${appKey}&type=public`;
-        }
-        //next link provided, just use that
-        else{
-            requestUrl = nextLink;
-        }
+        //collect the total number of recipes
+        setTotalRecipeCount(data.count);
 
-        //fetch from the API recipes that relate to those ingredients
-        try {
-
-            const response = fetch(requestUrl, {
-                'headers': headers,
-                'method': 'GET'
-            }).then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            }).then(data => {
-
-                setTotalRecipeCount(data.count);
-                setNextRecipesLink(data._links.next.href);
-
-                let recipes = []
-                if (data.hits.length > 0) {
-                    data.hits.map(item => {
-                        let recipe = item.recipe;
-                        let links = item._links;
-                        recipes.push(recipe);
-                    })
-                }
-                //set the recipes
-                setRecipes(recipes);
+        //search thoough all returned data to extract the recipes
+        let recipes = []
+        if (data.hits.length > 0) {
+            data.hits.map(item => {
+                let recipe = item.recipe;
+                let links = item._links;
+                recipes.push(recipe);
             })
-        } catch (error) {
-            throw new Error(`HTTP error! Status: ${error.message}`);
         }
+
+        //set the recipes
+        setRecipes(recipes);
     }
 
     ///Called when we have a recipe to show!
@@ -95,7 +58,7 @@ const RecipeGenerator = ({ingredients}) => {
 
         //we have 1 or more recipes, return them for display
         if (recipes.length > 0) {
-            return recipes.map((item,index) => {
+            return recipes.map((item, index) => {
                 const keyname = `recipe-item-${index}`;
                 return <Recipe data={item} key={keyname}/>
             })
@@ -148,7 +111,6 @@ const RecipeGenerator = ({ingredients}) => {
                             {ingredients.length >= minIngredientsRequired &&
                                 <div className="secondary">
                                     <button onClick={getRecipes}>Get a Recipe!</button>
-                                    <button onClick={testButton}>Test</button>
                                 </div>
                             }
                         </form>
@@ -156,7 +118,8 @@ const RecipeGenerator = ({ingredients}) => {
                     {recipes != null && recipes.length > 0 &&
                         <section className="recipes-section">
                             <h2>Look at all these recipes!</h2>
-                            <p>We've been able to find <strong>{totalRecipeCount}</strong> recipes that match your ingredients above. Listed below are <strong>{recipes.length}</strong> of them</p>
+                            <p>We've been able to find <strong>{totalRecipeCount}</strong> recipes that match your
+                                ingredients above. Listed below are <strong>{recipes.length}</strong> of them</p>
                             {getRecipeSection()}
                             <button onClick={getMoreRecipes}>Get more recipes!</button>
                         </section>
